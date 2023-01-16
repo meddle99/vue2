d49e97f41fc8d22e3f28861ad477da1844f55846
meddle99 <30960194+meddle99@users.noreply.github.com>
2023-01-15 19:31:39 -0500

    vue

---
+++ original-firmware/_ESP32 save flash 4MB.cmd
@@ -0,0 +1,2 @@
+esptool --port COM6 --baud 115200 read_flash 0 0x800000 flash_contents.bin
+pause
---
+++ original-firmware/_ESP32 write flash.cmd
@@ -0,0 +1,2 @@
+esptool --port COM4 --baud 921600 write_flash --erase-all 0x0000 emporia-vue2-esp32.bin
+pause
